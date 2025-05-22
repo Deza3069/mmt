97a599a0a575864f1b4dd048f92df73c1a3e4701
dod <hritikraj123412312@gmail.com>
2025-05-22 12:15:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 API_ID = int(os.getenv("API_ID", 25695415))
 API_HASH = os.getenv("API_HASH", "38a4b64f718fbe909cb54d083a7d1d46")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7582042900:AAG1IA4G_Lz0L90HszjfXlyCp4B1XDb5n6w")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7706239492:AAEUpfd0iq_soj2588Mpxg5dqX3WhuDxdOs")
 MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://Aizen:pro@cluster0.sqs6x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 OWNER_ID = int(os.getenv("OWNER_ID", 7580991572))
 LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", -1002538801686))
