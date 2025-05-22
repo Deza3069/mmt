fb3bd01115337260f830a188f5ccd540ce21b94b
dod <hritikraj123412312@gmail.com>
2025-05-22 11:11:02 +0530

    Create __init__.py

---
+++ database/__init__.py
@@ -0,0 +1,14 @@
+from .mongo import (
+    get_user,
+    add_sudo,
+    remove_sudo,
+    get_sudoers,
+    is_sudo,
+    get_owner_id,
+    get_user_smtps,
+    add_smtp,
+    delete_smtp,
+    count_smtps,
+    save_mail_record,
+    get_mail_records
+)
