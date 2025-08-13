54562d1bb4c4e752428a36cc83d5fd9e0687fa9f
Aɪᴢᴇɴ <hritikraj123412312@gmail.com>
2025-08-13 14:59:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 API_ID = int(os.getenv("API_ID", 25695415))
 API_HASH = os.getenv("API_HASH", "38a4b64f718fbe909cb54d083a7d1d46")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7706239492:AAEUpfd0iq_soj2588Mpxg5dqX3WhuDxdOs")
-MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://Aizen:pro@cluster0.sqs6x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://Aizen:Music@cluster0.dlbqwkc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 OWNER_ID = int(os.getenv("OWNER_ID", 7580991572))
 LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", -1002538801686))
